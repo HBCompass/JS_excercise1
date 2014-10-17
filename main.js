11503175dc26e33e9c54ed59a5e25772fe73f324
Hackbright Student <info@hackbrightacademy.com>
2014-10-16 18:30:02 -0700

    Changed text div to world.

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var getText = document.getElementById("get-text");
+getText.addEventListener('click',function(e){
+    e.preventDefault();
+    var theDiv = document.getElementById("text-box-replace").innerHTML = "World";
+    
+    //debugger;
+    
+});
+
+
+
+var greenDiv = document.getElementById("text-box-color");
+greenDiv.style.backgroundColor = 'red';
+
